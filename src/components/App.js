6e6dbf8c098ce794f28ce92639304ad6f6750927
Gideon <gideonkimaiyo254@gmail.com>
2025-07-10 08:52:23 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
+// src/components/App.js
+
 import React from "react";
+import NavBar from "./NavBar";    // ✅ default import
+import Home from "./Home";        // ✅ default import
+import About from "./About";      // ✅ default import
 
 function App() {
   return (
